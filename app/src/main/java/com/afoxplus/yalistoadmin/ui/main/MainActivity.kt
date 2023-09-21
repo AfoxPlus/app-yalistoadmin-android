fcd3553dcd5c2b89c69336fa384d1ea10f532184
Valentin Mendoza <jmendozat13@gmail.com>
2023-09-21 00:39:05 -0500

    Added core splashscreen

--- app/src/main/java/com/afoxplus/yalistoadmin/ui/main/MainActivity.kt
+++ app/src/main/java/com/afoxplus/yalistoadmin/ui/main/MainActivity.kt
@@ -3,16 +3,22 @@ package com.afoxplus.yalistoadmin.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.afoxplus.uikitcompose.ui.theme.UiKitComposeTheme
 import com.afoxplus.yalistoadmin.ui.graphs.RootNavigationGraph
+import com.afoxplus.yalistoadmin.ui.screens.splash.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
+    private val viewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { viewModel.isNavigate.value }
+        }
         setContent {
             UiKitComposeTheme {
                 RootNavigationGraph(navController = rememberNavController())
