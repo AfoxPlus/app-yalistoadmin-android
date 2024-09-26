b7dcaa8e16da92dbee85db06501f7f4bef26f1b1
Valentin Mendoza <jmendozat13@gmail.com>
2024-09-26 11:26:32 -0500

    :wrench: Fix tabs

--- app/src/main/java/com/afoxplus/yalistoadmin/delivery/screens/home/navbar/KitchenNavBarScreen.kt
+++ app/src/main/java/com/afoxplus/yalistoadmin/delivery/screens/home/navbar/KitchenNavBarScreen.kt
@@ -1,19 +1,29 @@
 package com.afoxplus.yalistoadmin.delivery.screens.home.navbar
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.afoxplus.uikit.designsystem.atoms.UIKitText
 import com.afoxplus.uikit.designsystem.foundations.UIKitTheme
 import com.afoxplus.uikit.designsystem.molecules.UIKitTabItem
-import com.afoxplus.uikit.designsystem.molecules.UIKitTabs
 import com.afoxplus.yalistoadmin.delivery.screens.ShareTabOrdersScreen
 import com.afoxplus.yalistoadmin.delivery.viewmodels.HomeViewModel
 import com.afoxplus.yalistoadmin.domain.entities.Order
+import kotlinx.coroutines.launch
 
 @Composable
 fun KitchenHomeScreen(
@@ -40,8 +50,52 @@ fun KitchenHomeScreen(
         }
     }
 
-    UIKitTabs(
+    KitchenStatesTabs(
         modifier = Modifier.padding(horizontal = UIKitTheme.spacing.spacing16),
         tabItems = tabItems.value
     ) { _, tabItem -> tabItem.screen() }
+}
+
+@Composable
+fun KitchenStatesTabs(
+    modifier: Modifier = Modifier,
+    tabItems: List<UIKitTabItem>,
+    onTabSelected: @Composable (index: Int, tabItem: UIKitTabItem) -> Unit
+) {
+    val pagerState = rememberPagerState { tabItems.size }
+    val selectedTabIndex = remember { derivedStateOf { pagerState.currentPage } }
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TabRow(
+            modifier = modifier,
+            selectedTabIndex = selectedTabIndex.value,
+            divider = {},
+            containerColor = Color.Transparent
+        ) {
+            tabItems.forEachIndexed { index, item ->
+                Tab(
+                    selected = selectedTabIndex.value == index,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(tabItems.indexOf(item))
+                        }
+                    },
+                    text = {
+                        UIKitText(
+                            text = item.title,
+                            style = UIKitTheme.typography.header05Bold
+                        )
+                    }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            onTabSelected(selectedTabIndex.value, tabItems[selectedTabIndex.value])
+        }
+    }
 }
